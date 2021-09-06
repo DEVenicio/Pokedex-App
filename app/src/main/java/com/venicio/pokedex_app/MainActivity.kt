2a898917f4933c29f95a8eb2cedf568023e7a3e7
DEVenicio <engineer.venicio@outlook.com>
2021-09-05 23:07:03 -0300

    add RecyclerView

--- app/src/main/java/com/venicio/pokedex_app/MainActivity.kt
+++ app/src/main/java/com/venicio/pokedex_app/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.venicio.pokedex_app
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.venicio.pokedex_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvPokemon.layoutManager = LinearLayoutManager(this)
+        binding.rvPokemon.setHasFixedSize(true)
     }
 }
